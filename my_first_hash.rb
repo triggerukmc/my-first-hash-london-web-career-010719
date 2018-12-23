c99f954802cc6823aac1ff1d0f5ff7572b62777c
Matthew Collins <matthew.collins1991@yahoo.com>
2018-12-23 23:00:37 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,9 +18,6 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
